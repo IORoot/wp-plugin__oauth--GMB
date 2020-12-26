87b8e75a18a44aea49a4bc69f8e862dec64b7994
Andy Pearson <andy@londonparkour.com>
2020-12-26 13:34:27 +0000

    Reorganise

--- src/acf/andyp_plugin_register.php
+++ src/acf/andyp_plugin_register.php
@@ -4,7 +4,7 @@ add_action( 'plugins_loaded', function() {
     do_action('register_andyp_plugin', [
         'title'     => 'ACF oAuth G.M.B. Button',
         'icon'      => 'google-my-business',
-        'color'     => '#3D5AFE',
+        'color'     => '#242424',
         'path'      => __FILE__,
     ]);
 } );
